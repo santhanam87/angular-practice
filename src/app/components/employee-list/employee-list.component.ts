04043d6017e99ebc97dfa48f33f2e02725d61a9a
Santhanam Elumalai <sansandy04@gmail.com>
2023-01-07 21:08:13 -0700

    adding plotly lib

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import * as Plotly from 'plotly.js';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.scss']
+  styleUrls: ['./employee-list.component.scss'],
 })
-export class EmployeeListComponent {
-
-}
+export class EmployeeListComponent {}
